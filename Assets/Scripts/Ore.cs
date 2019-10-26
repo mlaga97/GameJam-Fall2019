6bcd8c0f68b961c2d1fc30a820225ed059c2284f
Luc Lagarde <matieu.lagarde@gmail.com>
2019-10-25 21:59:39 -0500

    Handle ore regeneration

--- Assets/Scripts/Ore.cs
+++ Assets/Scripts/Ore.cs
@@ -6,10 +6,31 @@ public class Ore : MonoBehaviour {
   public string itemID;
   public int quantity;
 
+  public Sprite sprite1;
+  public Sprite sprite2;
+
+  public SpriteRenderer spriteRenderer;
+
   void Start() {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    spriteRenderer.sprite = sprite1;
+
+    StartCoroutine(Replenish());
+  }
+
+  IEnumerator Replenish() {
+    while (true) {
+      yield return new WaitForSeconds(1);
+      if (quantity <= 10)
+        quantity++;
+    }
   }
 
   void Update() {
+    if (quantity <= 0)
+      spriteRenderer.sprite = sprite2;
+    else
+      spriteRenderer.sprite = sprite1;
   }
 
   // TODO: Check tool (by reference)

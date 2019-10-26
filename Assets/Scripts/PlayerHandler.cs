550bcb864d380e35540085de8d7317f3298f0196
Luc Lagarde <matieu.lagarde@gmail.com>
2019-10-25 21:23:13 -0500

    Break MovementHandler out of PlayerHandler

--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -25,7 +25,6 @@ public class PlayerHandler : MonoBehaviour
 
   // Update is called once per frame
   void Update() {
-    HandleMovement();
     HandleInventory();
 
     if (currentTile && currentTile.tag == "Mineable" && Input.GetKey("f")) {
@@ -51,56 +50,5 @@ public class PlayerHandler : MonoBehaviour
     // Dump inventory if 
     if (Input.GetKeyDown("e"))
       inventory.logItems();
-  }
-
-  // TODO: Add intialWaitTime
-  void HandleMovement() {
-    string lastKey = currentKey;
-    currentKey = "";
-
-    // Check if we've been holding the key long enough to move again
-    bool moveThisTick = false || (currentKeyDuration > waitTime);
-
-    // Determine what key (if any) is being pressed
-    if (Input.GetKey("w"))
-      currentKey = "w";
-    else if (Input.GetKey("a"))
-      currentKey = "a";
-    else if (Input.GetKey("s"))
-      currentKey = "s";
-    else if (Input.GetKey("d"))
-      currentKey = "d";
-
-    if (currentKey != lastKey) {
-      // Reset if currentKey has changed
-      moveThisTick = true;
-      currentKeyDuration = 0;
-    } else if (currentKey != "") {
-      // Increment counter if still holding
-      currentKeyDuration++;
-    } else {
-      // Halt movement otherwise
-      moveThisTick = false;
-    }
-
-    // Handle Movement
-    if (moveThisTick) {
-      currentKeyDuration = 0; // Reset counter
-
-      switch (currentKey) {
-        case "w":
-          transform.Translate(0, 1, 0);
-          break;
-        case "a":
-          transform.Translate(-1, 0, 0);
-          break;
-        case "s":
-          transform.Translate(0, -1, 0);
-          break;
-        case "d":
-          transform.Translate(1, 0, 0);
-          break;
-      }
-    }
   }
 }

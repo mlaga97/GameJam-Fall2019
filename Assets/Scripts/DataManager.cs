7ba31182b12b715b238117e1a563637687da5d35
Luc Lagarde <matieu.lagarde@gmail.com>
2019-10-29 11:43:35 -0500

    Bug fixes

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
-  public static DataManager instance;
+  public bool reset = false;
 
   public Inventory inventory = new Inventory();
 
   public QuestProgress questProgress = new QuestProgress();
 
+  // Singleton stuff
+  public static DataManager instance;
   void Awake() {
-    if (instance == null)
+    if (instance == null) {
+      Debug.Log("Initializing game state!");
       instance = this;
+    } else if (instance != this) {
+      Debug.Log("Resetting game state!");
 
-    else if (instance != this)
+      // TODO: Reset better
+      instance.inventory = new Inventory();
+      instance.questProgress = new QuestProgress();
+
       Destroy(gameObject);
+    }
 
     DontDestroyOnLoad(gameObject);
   }
